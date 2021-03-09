2a03ec7d322771514624062d132b72ed04ee8927
DANNIEL <donghoon149@gmail.com>
2021-03-09 16:15:22 +0900

    fix: several comments

--- src/Programmers/NumberOfK.java
+++ src/Programmers/NumberOfK.java
@@ -3,14 +3,19 @@ package Programmers;
 
 import java.util.Arrays;
 
+/**
+ * k번째 수
+ * sorting 문제
+ **/
+
 public class NumberOfK {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = {};
         answer = new int[commands.length];
-        for(int i=0;i<commands.length;i++){
-            int[] temp = Arrays.copyOfRange(array,commands[i][0]-1, commands[i][1]);
+        for (int i = 0; i < commands.length; i++) {
+            int[] temp = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]);
             Arrays.sort(temp);
-            answer[i] = temp[commands[i][2]-1];
+            answer[i] = temp[commands[i][2] - 1];
         }
         return answer;
     }
